968d0a4226fa45161c90f89dc7f9e9b932b89936
Marilyn Martha Yusnita Devi Parhusip <marilyn.parhusip@gmail.com>
2019-07-05 14:36:40 +0700

    Initial Commit

---
+++ UserDeffault/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserDeffault
+//
+//  Created by Marilyn Martha Yusnita Devi Parhusip on 05/07/19.
+//  Copyright © 2019 Apple Academy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
